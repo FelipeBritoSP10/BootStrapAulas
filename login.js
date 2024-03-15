93ba8a79876c37c05251aa9eaf7207f91eac8956
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-03-15 12:40:50 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,1 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-   
-    let username = document.getElementById('username').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-  
-    if (username.trim() !== '' && email.trim() !== '' && password.trim() !== '') {
-    
-      window.location.href = './index.html';
-    } else {
-      alert('Por favor, preencha todos os campos.');
-    }
-  });
+
