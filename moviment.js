e0a8bbe711212a3cc82ae668df2cac4e33043171
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-03-16 12:52:45 -0300

    Update moviment.js

--- moviment.js
+++ moviment.js
@@ -1,4 +1,21 @@
                         //Modal
-document.getElementById('open-modal').addEventListener('click', function() {
+    document.getElementById('open-modal').addEventListener('click', function() {
     $('#myImageModal').modal('show');
   });
+    document.getElementById('open-modal').addEventListener('click', function() {
+    $('#myImageModal').modal('show');
+  });
+    document.getElementById('subscriptionForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário por padrão
+
+    // Obtenha os valores dos campos do formulário
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    const body = `Nome: ${nome}%0D%0AEmail: ${email}%0D%0AMensagem: ${mensagem}`;
+    
+    window.location.href = `https://api.staticforms.xyz/submit?name=${nome}&email=${email}&message=${mensagem}&accessKey=bbd8a963-df97-4a9a-9652-c55b03f58ab1`;
+    
+    document.getElementById('mensagemSucesso').style.display = 'block';
+});
