d8c41f9e29b43363c3b379e23fdcb6434cce5585
Lucas bezerra <140905557+lucasbezerrapro@users.noreply.github.com>
2024-03-17 21:17:55 -0300

    novas alterações

---
+++ js/moviment.js
@@ -0,0 +1,28 @@
+              //Modal
+  document.getElementById('open-modal').addEventListener('click', function() {
+  $('#myImageModal').modal('show');
+  });
+
+          //Inscrição
+  const token = 'bbd8a963-df97-4a9a-9652-c55b03f58ab1';
+
+  const formData = new FormData();
+  formData.append('name', 'Nome do usuário');
+  formData.append('email', 'exemplo@email.com');
+  formData.append('message', 'Enviado');
+  formData.append('accessKey', token);
+  
+  fetch('https://api.staticforms.xyz/submit', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      document.write('Formulário enviado com sucesso!');
+    } else {
+      console.error('Erro ao enviar formulário:', response.statusText);
+    }
+  })
+  .catch(error => {
+    console.error('Erro:', error);
+  }); 
